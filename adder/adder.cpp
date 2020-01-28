4a505866596d2a7b563a3d6029675b3a667a9546
Viktor Khristenko <vdkhristenko1991@gmail.com>
2020-01-28 13:22:53 +0100

    add port directives

--- adder/adder.cpp
+++ adder/adder.cpp
@@ -1,6 +1,11 @@
 #include "adder.hpp"
 
 int adder(int in1, int in2, int in3) {
+#pragma HLS INTERFACE ap_ctrl_none port=return
+#pragma HLS INTERFACE ap_none port=in3
+#pragma HLS INTERFACE ap_none port=in2
+#pragma HLS INTERFACE ap_none port=in1
+
     int sum;
 
     sum = in1 + in2 + in3;
